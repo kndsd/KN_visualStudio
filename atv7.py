0dc1223df12d89234215d1c9d927b15f05572bea
kndsd <Kauenatanael000@gmail.com>
2025-08-29 12:28:15 -0300

    Update atv7.py

--- atv7.py
+++ atv7.py
@@ -9,7 +9,19 @@ imc = peso/(altura*altura)
 if imc < 18.5 :
     print("!!peso baixo!!") 
 
-elif imc <= 18.5:    
+elif 18.6 <= imc <= 24.9:    
     print("peso normal")
+
+elif  25.0 <= imc <= 29.9:
+    print("peso levimente asima")
+
+elif 30.0 <= imc <= 34.9:
+      print ("obesidade 1")
+
+elif 35.0 <= imc <= 39.9:
+     print("obesidade 2") 
+
+else: 
+  print("obesidade 3")
 
     
